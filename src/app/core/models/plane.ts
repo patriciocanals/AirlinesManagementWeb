fce3bb1643574cb1f47d6a5aabe042c1f8426fac
Patricio <it1@gestionglobalprint.com>
2025-06-14 21:38:49 +0200

    created plane interface and route interface

---
+++ src/app/core/models/plane.ts
@@ -0,0 +1,11 @@
+export interface Plane {
+    id: string;
+    model: string;
+    capacity: number;
+    speed: number;
+    range: number;
+    caegory: number;
+    consumption: number;
+    wear: number;
+    image: string;
+}
---
+++ src/app/core/models/route.ts
@@ -0,0 +1,9 @@
+export interface Route {
+    id: string;
+    origin: string;
+    iataOrigin: string;
+    destination: string;
+    iataDestination: string;
+    distance: number;
+    frequency: number;
+}
